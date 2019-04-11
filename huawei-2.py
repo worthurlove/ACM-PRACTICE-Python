b146fbb7f075a3fb994fcc034e877d4bfe254f7d
worthurlove <2087447114@qq.com>
2019-04-11 14:59:01 +0800

    考虑多位数的情况

--- huawei-2.py
+++ huawei-2.py
@@ -1,6 +1,21 @@
-N = 'abc3(A)asd2(asd1(er))'
+'''
+leetcode系列：题号-394
+Description:Given an encoded string, return it's decoded string.
 
-N = input()
+        The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+        You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+        Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+        华为笔试题改编要求逆序输出，但是不考虑多位数的情况
+Author:worthurlove
+Date:2019.4.11
+'''
+
+
+s = 'abc10(A)'
+
 
 #定义一个栈
 
@@ -8,61 +23,63 @@ stack_bracket = []
 
 stack_num = []
 
-rep = [0]*100
-
 i = 0
-j = 0#重复的字符串
 
-while i < len(N):
-
-    if str(N[i]).isdigit():
-        stack_num.append(int(N[i]))
-        stack_bracket.append(str(N[i + 1]))
+while i < len(s):
+    
+    if str(s[i]).isdigit():
+        nums = int(s[i])
+        #考虑多位数的情况
+        while str(s[i + 1]).isdigit():
+            nums = nums*10 + int(s[i + 1])
+            i += 1
+        stack_num.append(nums)
+        stack_bracket.append(str(s[i + 1]))
         i += 2
-        while str(N[i]).isalpha():
-            stack_bracket.append(str(N[i]))
+        while str(s[i]).isalpha():
+            stack_bracket.append(str(s[i]))
             i += 1
 
 
-    elif str(N[i]) == ')':
-            s = []
+    elif str(s[i]) == ')':
+            part = []
             i += 1
             while stack_bracket[-1] != '(':
-                s.insert(0,stack_bracket.pop())
+                part.insert(0,stack_bracket.pop())
              
             stack_bracket.pop()
 
             for k in range(int(stack_num.pop())):
-                stack_bracket.extend(s)
-    elif str(N[i]) == ']':
-            s = []
+                stack_bracket.extend(part)
+    elif str(s[i]) == ']':
+            part = []
             i += 1
             while stack_bracket[-1] != '[':
-                s.insert(0,stack_bracket.pop())
+                part.insert(0,stack_bracket.pop())
              
             stack_bracket.pop()
 
             for k in range(int(stack_num.pop())):
-                stack_bracket.extend(s)
-    elif str(N[i]) == '}':
-            s = []
+                stack_bracket.extend(part)
+    elif str(s[i]) == '}':
+            part = []
             i += 1
             while stack_bracket[-1] != '{':
-                s.insert(0,stack_bracket.pop())
+                part.insert(0,stack_bracket.pop())
              
             stack_bracket.pop()
 
             for k in range(int(stack_num.pop())):
-                stack_bracket.extend(s)
+                stack_bracket.extend(part)
     else:
-        stack_bracket.append(str(N[i]))
+        stack_bracket.append(str(s[i]))
         i += 1
 
 
-s = ''
+part = ''
 for i in stack_bracket:
-    s = i + s
-print(s)
+    part = i + part
+print(part)
 
                 
 
